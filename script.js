b91daf436765af0b15d230f5592c31d497ee1b64
John O Raab <125783786+jrsr5599@users.noreply.github.com>
2023-05-22 11:07:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let currentDate = dayjs(); 
+let displayDate = $("#currentDay");
+displayDate.text(currentDate.format("MMM D, YYYY")); 
+
+
+
+$(document).ready(function () {
+  let saveButtons = $(".saveBtn");
+ 
+  saveButtons.on("click", function (event) {
+  
+    let saveButton = $(this);
+  
+    let timeBlock = saveButton.closest(".time-block");
+  
+    let id = timeBlock.attr("id");
+    
+    let description = timeBlock.find(".description").val();
+    // Save event in local storage
+    localStorage.setItem(id, JSON.stringify(description));
+  });
+
+  
+  $('.time-block').each(function() {
+    let id = $(this).attr("id");
+    let description = localStorage.getItem(id);
+    if (description) {
+      $(this).find('.description').val(JSON.parse(description));
+    }
+  });
+
+
+// Setting the classes to past, present and future
+  function trackTime() {
+    let currentHour = new Date().getHours();
+    let timeBlocks = $(".time-block");
+   
+
+    timeBlocks.each(function () {
+      let time = $(this).attr("id").split("hour")[1];
+      if (currentHour == time) {
+        $(this).addClass("present");
+      } else if (currentHour < time) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > time) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+  }
+  trackTime();
+});
